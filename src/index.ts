99d8a1ddcb96efaf3bc5f49d416ab5b766951f2a
Manish Pant <manishpant203@gmail.com>
2024-10-13 16:27:44 +0530

    feat: support different delimiters //[delimiter]\n[numbers…]

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,17 @@ class strCalculator {
   add(numbers: string): number {
     if (!numbers) return 0; // handle empty string case
 
-    const delimiter = /[,\n]/;  // default newline & comma
+    let delimiter = /[,\n]/; // default newline & comma
+
+    //for //[delimiter]\n[numbers…]
+    if (numbers.startsWith("//")) {
+      const delimiterBeforeNewLine = numbers.substring(
+        2,
+        numbers.indexOf("\n")
+      );
+      delimiter = new RegExp(`[,\n${delimiterBeforeNewLine}]`); // update delimiter with new delimiter
+      numbers = numbers.substring(numbers.indexOf("\n") + 1); // numbers after /n "new line"
+    }
 
     const numbersArray = numbers.split(delimiter).map(Number); // convert str in array
     return numbersArray.reduce((sum, num) => sum + num, 0);
@@ -10,8 +20,9 @@ class strCalculator {
 }
 
 const calculator = new strCalculator();
-console.log(calculator.add(''))
-console.log(calculator.add('1'))
-console.log(calculator.add('2,3'))
-console.log(calculator.add('2,3,7,5'))
-console.log(calculator.add('1\n2,3'))
+console.log(calculator.add(""));
+console.log(calculator.add("1"));
+console.log(calculator.add("2,3"));
+console.log(calculator.add("2,3,7,5"));
+console.log(calculator.add("1\n2,3"));
+console.log(calculator.add("//|\n4|5|6"));
