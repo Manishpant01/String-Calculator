72a3c0b9e214b8a44d4138a73c903cc485adfd9c
Manish Pant <manishpant203@gmail.com>
2024-10-13 15:31:25 +0530

    feat: add test cases

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
 //Info: steps to be taken for creating str calculator
 
-// steps
-// 1. a function that take input a string of comma-separated numbers and return output as an integer, sum of the numbers 
+// steps:-
+// 1. a function that take input a string of comma-separated numbers and return output as an integer, sum of the numbers
     // Input: “”, Output: 0
     // Input: “1”, Output: 1
     // Input: “1,5”, Output: 6
@@ -13,3 +13,26 @@
 // 7. should support different delimiters "//;\n"
 // 8. should throw an error for negative numbers
 // 9. should throw an error for multiple negative numbers
+
+describe("StringCalculator", () => {
+  beforeEach(() => {});
+
+  it('should handle empty string case', () => {
+    expect(calculator.add('')).toBe(0);
+  });
+
+  it('should handle single numbers and return a same integer number', () => {
+    expect(calculator.add('1')).toBe(1);
+    expect(calculator.add('5')).toBe(5);
+  });
+
+  it('should handle two comma-separated numbers and return sum of them', () => {
+    expect(calculator.add('1,2')).toBe(3);
+    expect(calculator.add('3,5')).toBe(8);
+  });
+
+  it('should handle an multiple amount of numbers and return sum of them', () => {
+    expect(calculator.add('1,2,3,4')).toBe(10);
+    expect(calculator.add('5,10,15')).toBe(30);
+  });
+});
