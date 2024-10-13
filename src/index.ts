92ccdf0ba27e527c4388dddb74c28dd6e3d2fb86
Manish Pant <manishpant203@gmail.com>
2024-10-13 15:56:15 +0530

    feat: handle single numbers and two comma-separated numbers and return sum of them

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 class strCalculator {
-    add(numbers: string): number {
-        if (!numbers) return 0; // handle empty string case
-    }
+  add(numbers: string): number {
+    if (!numbers) return 0; // handle empty string case
+
+    const delimiter = ",";
+
+    const numbersArray = numbers.split(delimiter).map(Number); // convert str in array
+    return numbersArray.reduce((sum, num) => sum + num, 0);
+  }
 }
+
+const calculator = new strCalculator();
+console.log(calculator.add(''))
+console.log(calculator.add('1'))
+console.log(calculator.add('2,3'))
