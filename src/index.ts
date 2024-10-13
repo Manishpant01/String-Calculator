7048237b2f6e25e21af370000fe2af2544dfc3f2
Manish Pant <manishpant203@gmail.com>
2024-10-13 16:03:15 +0530

    feat: add default delimiter for newline & comma

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ class strCalculator {
   add(numbers: string): number {
     if (!numbers) return 0; // handle empty string case
 
-    const delimiter = ",";
+    const delimiter = /[,\n]/;  // default newline & comma
 
     const numbersArray = numbers.split(delimiter).map(Number); // convert str in array
     return numbersArray.reduce((sum, num) => sum + num, 0);
@@ -13,3 +13,5 @@ const calculator = new strCalculator();
 console.log(calculator.add(''))
 console.log(calculator.add('1'))
 console.log(calculator.add('2,3'))
+console.log(calculator.add('2,3,7,5'))
+console.log(calculator.add('1\n2,3'))
