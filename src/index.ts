73b475d772ac3851462cf52a47341dc6692f1234
Manish Pant <manishpant203@gmail.com>
2024-10-21 12:05:40 +0530

    feat: add logic for handle single numbers and return a same integer number

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 class strCalculator {
   add(numbers :string){
     if (!numbers) return 0; // handle empty string case
+
+    const numbersArray = numbers.split(',').map(Number);
+    return numbersArray.reduce((sum, num) => sum + num, 0);
+
   }
+
+
 }
 
 const calculator = new strCalculator();
-console.log(calculator.add(''))
+console.log(calculator.add('2'))
 
 export { strCalculator };
