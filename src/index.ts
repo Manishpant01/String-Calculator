70a26eb6c2ecafc9e83b9b5bd11a1e66a0ac4158
Manish Pant <manishpant203@gmail.com>
2024-10-21 11:58:22 +0530

    feat: add logic for handle empty string case

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 class strCalculator {
-
+  add(numbers :string){
+    if (!numbers) return 0; // handle empty string case
+  }
 }
 
+const calculator = new strCalculator();
+console.log(calculator.add(''))
 
 export { strCalculator };
