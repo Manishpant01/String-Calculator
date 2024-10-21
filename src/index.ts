da9fbed948466d16fc2d1cba03065cd908c5d514
Manish Pant <manishpant203@gmail.com>
2024-10-21 12:29:52 +0530

    feat: add logic for handle support different delimiters //[delimiter]\n[numbers…]

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 class strCalculator {
-  add(numbers :string){
+  add(numbers: string) {
     if (!numbers) return 0; // handle empty string case
 
     let delimiter = /[,\n]/; // for newline & comma
 
+    //for //[delimiter]\n[numbers…]
+    if (numbers.startsWith("//")) {
+      const delimiterBeforeNewLine = numbers.substring(
+        2,
+        numbers.indexOf("\n")
+      );
+      delimiter = new RegExp(`[,\n${delimiterBeforeNewLine}]`); // update delimiter with new delimiter
+      numbers = numbers.substring(numbers.indexOf("\n") + 1); // numbers after /n "new line"
+    }
+
     const numbersArray = numbers.split(delimiter).map(Number);
     return numbersArray.reduce((sum, num) => sum + num, 0);
   }
-
-
 }
 
 const calculator = new strCalculator();
-console.log(calculator.add('2'))
+console.log(calculator.add("2"));
 
 export { strCalculator };
